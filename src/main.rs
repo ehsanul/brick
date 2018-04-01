39d422c6493ade47280049dd962f6eb3e7fa7793
Ehsanul Hoque <ehsanul@ehsanul.com>
2018-04-01 14:38:08 -0700

    record ball position in file + accurate car/ball dimensions

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ extern crate lazy_static;
 pub mod game_data;
 pub mod game_data_grpc;
 
+use std::io::prelude::*;
+use std::fs::File;
 use std::thread;
 use std::sync::{RwLock};
 use std::f32;
@@ -29,23 +31,36 @@ struct GameState {
 }
 
 struct PlayerState {
-    coordinates: Vector3<f32>,
+    position: Vector3<f32>,
     rotation: UnitQuaternion<f32>,
 }
 
 struct BallState {
-    coordinates: Vector3<f32>,
+    position: Vector3<f32>,
 }
+
+static BALL_RADIUS: f32 = 93.143;
 
 
 lazy_static! {
+    // batmobile
+    static ref CAR_DIMENSIONS: Vector3<f32> = Vector3::new(128.82, 84.67, 29.39);
+    static ref PIVOT_OFFSET: Vector3<f32> = Vector3::new(9.008, 0.0, 12.094);
+
     static ref GAME_STATE: RwLock<GameState> = {
         RwLock::new(GameState {
-            ball: BallState { coordinates: Vector3::new(0.0, 0.0, 0.0) },
-            player: PlayerState { coordinates: Vector3::new(0.0, 0.0, 0.0), rotation: UnitQuaternion::from_euler_angles(0.0, 0.0, 0.0) },
+            ball: BallState { position: Vector3::new(0.0, 0.0, 0.0) },
+            player: PlayerState { position: Vector3::new(0.0, 0.0, 0.0), rotation: UnitQuaternion::from_euler_angles(0.0, 0.0, 0.0) },
         })
     };
+
+    static ref FILE: RwLock<File> = {
+        RwLock::new(File::create("foo.csv").unwrap())
+    };
 }
+
+static mut record: bool = true;
+
 
 struct BotImpl;
 
@@ -63,35 +78,37 @@ impl Bot for BotImpl {
         let player = &packet.players[packet.player_index as usize];
 
         let bl = ball.get_location();
-        game_state.ball.coordinates = Vector3::new(-bl.x, bl.y, bl.z); // x should be positive towards right, it only makes sense
+        game_state.ball.position = Vector3::new(-bl.x, bl.y, bl.z); // x should be positive towards right, it only makes sense
 
         let pl = player.get_location();
         let pr = player.get_rotation();
-        game_state.player.coordinates = Vector3::new(-pl.x, pl.y, pl.z); // x should be positive towards right, it only makes sense
+        game_state.player.position = Vector3::new(-pl.x, pl.y, pl.z); // x should be positive towards right, it only makes sense
         game_state.player.rotation = UnitQuaternion::from_euler_angles(-pr.roll, pr.pitch, -pr.yaw);
 
-        // slightly modified atba, but broken when ball is towards blue side relative to player :/
-        /*
+        let mut file = FILE.write().unwrap();
+        unsafe {
+            if record {
+                file.write_all(format!("{},{},{},{},{},{},{},{},{},{},{},{},{}\n",
+                    packet.get_game_info().seconds_elapsed,
+                    -bl.x,
+                    bl.y,
+                    bl.z,
+                    -ball.get_velocity().x,
+                    ball.get_velocity().y,
+                    ball.get_velocity().z,
+                    -ball.get_acceleration().x,
+                    ball.get_acceleration().y,
+                    ball.get_acceleration().z,
+                    -ball.get_angular_velocity().x,
+                    ball.get_angular_velocity().y,
+                    ball.get_angular_velocity().z,
+                ).as_bytes());
+                //println!("ball z: {}", bl.z);
+            }
+        }
+
         controller_state.throttle = 1.0;
-
-        // note x-axis is flipped when blue facing orange (left is +x), hence negate
-        let mut xd = -(ball.get_location().x - player.get_location().x);
-        let yd = ball.get_location().y - player.get_location().y;
-
-        // naieve, but: if ball is to left of goal, go more left
-        if ball.get_location().x > 0.0 { xd -= 40.0 }
-        // naieve, but: if ball is to right of goal, go more right
-        if ball.get_location().x < 0.0 { xd += 40.0 }
-
-        let mut theta = yd.atan2(xd);
-        let angle = f32::consts::PI - theta;
-        //println!("yd: {}, xd: {}, angle: {}; theta: {}, yaw: {}", yd, xd, angle, theta, player.get_rotation().yaw);
-        //if player.get_rotation().yaw < angle {
-            controller_state.steer = 1.0;
-        //} else {
-        //    controller_state.steer = -1.0;
-        //}
-        */
+        //controller_state.steer = 1.0;
 
         grpc::SingleResponse::completed(controller_state)
     }
@@ -101,27 +118,30 @@ fn main() {
     // visualization
     thread::spawn(move || {
         let mut window = Window::new("Rocket League Visualization");
-        let mut sphere = window.add_sphere(0.1);
-        let mut car = window.add_cube(0.2, 0.1, 0.05);
+
+        // we're dividing everything by 1000 until we can set the camera up to be more zoomed out
+        let mut sphere = window.add_sphere(BALL_RADIUS / 1000.0);
+        let mut car = window.add_cube(CAR_DIMENSIONS.x/1000.0, CAR_DIMENSIONS.y/1000.0, CAR_DIMENSIONS.z/1000.0);
 
         sphere.set_color(0.8, 0.8, 0.8);
         car.set_color(0.1, 0.4, 1.0);
 
         window.set_light(Light::StickToCamera);
 
-        let mut floor = window.add_cube(8.0, 10.0, 0.01);
+        let mut floor = window.add_cube(8.0, 10.0, 0.001);
         floor.set_surface_rendering_activation(false);
         floor.set_points_size(0.1);
         floor.set_lines_width(0.1);
         while window.render() {
             let game_state = &GAME_STATE.read().unwrap();
 
-            // we're dividing coordinates by 1000 until we can set the camera up to be more zoomed out
-            sphere.set_local_translation(Translation3::from_vector(game_state.ball.coordinates.map(|c| c / 1000.0)));
+            // we're dividing position by 1000 until we can set the camera up to be more zoomed out
+            sphere.set_local_translation(Translation3::from_vector(game_state.ball.position.map(|c| c / 1000.0)));
 
-            // we're dividing coordinates by 1000 until we can set the camera up to be more zoomed out
-            car.set_local_translation(Translation3::from_vector(game_state.player.coordinates.map(|c| c / 1000.0)));
-            car.set_local_rotation(game_state.player.rotation);
+            // we're dividing position by 1000 until we can set the camera up to be more zoomed out
+            let hitbox_position = game_state.player.position.map(|c| c / 1000.0) + PIVOT_OFFSET.map(|c| c / 1000.0);
+            car.set_local_translation(Translation3::from_vector(hitbox_position));
+            car.set_local_rotation(game_state.player.rotation); // FIXME need to rotate about the pivot, not center
         }
     });
 
