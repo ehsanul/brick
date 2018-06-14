0372f29bdad82642e424a3f67cab68d0b75f08db
Ehsanul Hoque <ehsanul@ehsanul.com>
2018-06-14 10:01:53 -0400

    minimal-est

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,17 @@
-extern crate dynamic_reload;
-
-#[macro_use]
-extern crate lazy_static;
-
-use std::sync::{Arc, Mutex};
-use std::f32;
-use dynamic_reload::{DynamicReload, Lib, Symbol, Search, PlatformName, UpdateState};
-
-lazy_static! {
-    static ref RELOAD_HANDLER: Mutex<DynamicReload<'static>> = {
-        Mutex::new(
-            DynamicReload::new(Some(vec!["predict/target/debug"]),
-                               Some("target/debug"),
-                               Search::Default)
-        )
-    };
-
-    static ref PREDICT: Mutex<PredictPlugin> = {
-        let lib = match RELOAD_HANDLER.lock().expect("Failed to get lock on RELOAD_HANDLER").add_library("predict", PlatformName::Yes) {
-            Ok(lib) => lib,
-            Err(e) => {
-                panic!("Unable to load dynamic lib, err {:?}", e);
-            }
-        };
-        Mutex::new(PredictPlugin { lib: Some(lib) })
-    };
-}
-
-
-struct PredictPlugin {
-    lib: Option<Arc<Lib>>
-}
-
-impl PredictPlugin {
-    fn unload_plugin(&mut self, lib: &Arc<Lib>) {
-        self.lib = None;
-    }
-
-    fn reload_plugin(&mut self, lib: &Arc<Lib>) {
-        self.lib = Some(lib.clone());
-    }
-
-    fn reload_callback(&mut self, state: UpdateState, lib: Option<&Arc<Lib>>) {
-        match state {
-            UpdateState::Before => Self::unload_plugin(self, lib.unwrap()),
-            UpdateState::After => Self::reload_plugin(self, lib.unwrap()),
-            UpdateState::ReloadFailed(_) => println!("Failed to reload"),
-        }
-    }
-}
-
+//use std::f32;
+extern crate libloading as lib;
 
 fn main() {
-    // visualization
-    loop {
-        // update real time
-        //{
-        //    // XXX there must be a reason why this happens, but PREDICT must be locked before
-        //    // RELOAD_HANDLER, otherwise we apparently end up in a deadlock
-        //    let mut p = PREDICT.lock().expect("Failed to get lock on PREDICT");
-        //    let mut rh = RELOAD_HANDLER.lock().expect("Failed to get lock on RELOAD_HANDLER");
-        //    rh.update(PredictPlugin::reload_callback, &mut p);
-        //}
-        if let Some(ref x) = PREDICT.lock().unwrap().lib {
-            let test_it: Symbol<extern "C" fn() -> Vec<f32>> = unsafe {
-                x.lib.get(b"test_it\0").unwrap()
-            };
-            loop {
-                let v = test_it();
-                println!("test_it: {:?}", v);
-            }
+    if let Ok(lib) = lib::Library::new("predict/target/debug/libpredict.dylib") {
+        let test_it: lib::Symbol<extern fn() -> Vec<f32>> = unsafe {
+            lib.get(b"test_it\0").unwrap()
+        };
+
+        loop {
+            let v = test_it();
+            println!("test_it: {:?}", v);
         }
+    } else {
+        println!("couldn't find libpredict.dylib. Are you on osx and did you compile the predict sub-crate?")
     }
 }
