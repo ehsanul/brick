d4147ba68ed8520e12413b7c25c05a37651410ee
Ehsanul Hoque <ehsanul@ehsanul.com>
2018-06-15 22:36:05 -0400

    debugging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-//use std::f32;
+// XXX uncomment below to use system allocator
+//
+// #![cfg_attr(rustc_nightly, feature(test))]
+// #![feature(alloc_system)]
+//
+// #![feature(alloc_system)]
+// #![feature(global_allocator, allocator_api)]
+//
+// extern crate alloc_system;
+//
+// use alloc_system::System;
+//
+// #[global_allocator]
+// static A: System = System;
+
+use std::mem;
 extern crate libloading as lib;
 
 fn main() {
@@ -10,6 +25,7 @@ fn main() {
         loop {
             let v = test_it();
             println!("test_it: {:?}", v);
+            //mem::forget(v); // <-- leaking `v` avoids the segfault
         }
     } else {
         println!("couldn't find libpredict.dylib. Are you on osx and did you compile the predict sub-crate?")
