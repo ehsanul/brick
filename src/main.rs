7e844494bb7ad95f89ca15dd50eac8c6b3e52efe
Ehsanul Hoque <ehsanul@ehsanul.com>
2018-07-08 23:36:00 -0700

    more cleanup + use rlbot bindings

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate kiss3d;
 extern crate nalgebra as na;
 extern crate dynamic_reload;
 extern crate state;
+extern crate rlbot;
 
 #[macro_use]
 extern crate lazy_static;
@@ -140,65 +141,86 @@ impl Bot for BotImpl {
 }
 */
 
-fn main() {
-    // visualization
-    thread::spawn(move || {
-        let mut window = Window::new("Rocket League Visualization");
+fn run_visualization(){
+    let mut window = Window::new("Rocket League Visualization");
 
-        // we're dividing everything by 1000 until we can set the camera up to be more zoomed out
-        let mut sphere = window.add_sphere(BALL_RADIUS / 1000.0);
-        let mut car = window.add_cube(CAR_DIMENSIONS.x/1000.0, CAR_DIMENSIONS.y/1000.0, CAR_DIMENSIONS.z/1000.0);
+    // we're dividing everything by 1000 until we can set the camera up to be more zoomed out
+    let mut sphere = window.add_sphere(BALL_RADIUS / 1000.0);
+    let mut car = window.add_cube(CAR_DIMENSIONS.x/1000.0, CAR_DIMENSIONS.y/1000.0, CAR_DIMENSIONS.z/1000.0);
 
-        let arena_mesh = MeshManager::load_obj(
-                            Path::new("./assets/arena.obj"),
-                            Path::new("./assets/"),
-                            "arena"
-                        ).expect("Can't load arena obj file")
-                        .pop().expect("Missing arena mesh")
-                        .1
-                        .clone();
-        let mut arena = window.add_mesh(arena_mesh, Vector3::new(0.0, 0.0, 0.0));
-        arena.set_surface_rendering_activation(false);
-        arena.set_points_size(0.1);
-        arena.set_lines_width(0.1);
-        arena.set_local_scale(0.001, 0.001, 0.001);
+    let arena_mesh = MeshManager::load_obj(
+                        Path::new("./assets/arena.obj"),
+                        Path::new("./assets/"),
+                        "arena"
+                    ).expect("Can't load arena obj file")
+                    .pop().expect("Missing arena mesh")
+                    .1
+                    .clone();
+    let mut arena = window.add_mesh(arena_mesh, Vector3::new(0.0, 0.0, 0.0));
+    arena.set_surface_rendering_activation(false);
+    arena.set_points_size(0.1);
+    arena.set_lines_width(0.1);
+    arena.set_local_scale(0.001, 0.001, 0.001);
 
-        sphere.set_color(0.8, 0.8, 0.8);
-        car.set_color(0.1, 0.4, 1.0);
+    sphere.set_color(0.8, 0.8, 0.8);
+    car.set_color(0.1, 0.4, 1.0);
 
-        window.set_light(Light::StickToCamera);
+    window.set_light(Light::StickToCamera);
 
-        while window.render() {
-            let game_state = &GAME_STATE.read().unwrap();
+    while window.render() {
+        let game_state = &GAME_STATE.read().unwrap();
 
-            // we're dividing position by 1000 until we can set the camera up to be more zoomed out
-            sphere.set_local_translation(Translation3::from_vector(game_state.ball.position.map(|c| c / 1000.0)));
+        // we're dividing position by 1000 until we can set the camera up to be more zoomed out
+        sphere.set_local_translation(Translation3::from_vector(game_state.ball.position.map(|c| c / 1000.0)));
 
-            // we're dividing position by 1000 until we can set the camera up to be more zoomed out
-            let hitbox_position = game_state.player.position.map(|c| c / 1000.0) + PIVOT_OFFSET.map(|c| c / 1000.0);
-            car.set_local_translation(Translation3::from_vector(hitbox_position));
-            car.set_local_rotation(game_state.player.rotation); // FIXME need to rotate about the pivot, not center
-        }
-    });
+        // we're dividing position by 1000 until we can set the camera up to be more zoomed out
+        let hitbox_position = game_state.player.position.map(|c| c / 1000.0) + PIVOT_OFFSET.map(|c| c / 1000.0);
+        car.set_local_translation(Translation3::from_vector(hitbox_position));
+        car.set_local_rotation(game_state.player.rotation); // FIXME need to rotate about the pivot, not center
+    }
+}
 
+/// main bot playing loop
+fn run_bot() {
+    loop {
+        let player_index = *PLAYER_INDEX.lock().unwrap();
+        //println!("player index: {:?}", player_index);
+        let player_index = match player_index {
+            Some(i) => i,
+            None => {
+                thread::sleep_ms(1000);
+                continue;
+            }
+        };
 
-    thread::spawn(|| {
-        loop {
-            println!("player index: {:?}", *PLAYER_INDEX.lock().unwrap());
-            thread::sleep_ms(1000); // TODO measure time taken by bot and do diff
-        }
-    });
+        let mut packet = rlbot::LiveDataPacket::default();
+        rlbot::update_live_data_packet(&mut packet);
+        //println!("{:?}", packet.GameBall);
 
-    // obtain port to communicate with python agent. must match the port the python agent is configured to send to!
-    let mut port_file = File::open("port.cfg").expect("port.cfg file not found");
+        let mut input = rlbot::PlayerInput::default();
+        input.Steer = 1.0;
+        input.Throttle = 0.5;
+        rlbot::update_player_input(input, player_index as i32);
+
+        thread::sleep_ms(1000 / 120); // TODO measure time taken by bot and do diff
+    }
+}
+
+// obtain port to communicate with python agent. must match the port the python agent is configured to send to!
+fn get_port(filename: &str) -> u16 {
+    let mut port_file = File::open(filename).expect(&format!("{} file not found", filename));
     let mut contents = String::new();
     port_file.read_to_string(&mut contents).expect("something went wrong reading the port.cfg file");
-    let port = contents.trim().parse::<u16>().expect(&format!("couldn't parse port: {}", contents));
+    contents.trim().parse::<u16>().expect(&format!("couldn't parse port: {}", contents))
+}
 
-    // super basic tcp server. only used to get the right index from the python agent for now.
+/// super basic tcp server. only used to get the right index from the python agent for now.
+fn run_server() {
+    let port = get_port("port.cfg");
     let listener = TcpListener::bind(("127.0.0.1", port)).expect(&format!("Failed to bind port {}", port));
     let mut message = String::new();
     for stream in listener.incoming() {
+        message.clear();
         let mut stream = stream.expect("Failed to accept connection");
         stream.read_to_string(&mut message).expect("Couldn't read tcp message to utf8 string");
         let mut split_message = message.split_whitespace();
@@ -217,5 +239,14 @@ fn main() {
             "remove" => *PLAYER_INDEX.lock().unwrap() = None,
             _ => unimplemented!(),
         };
+        println!("---------------------------------------------");
+        println!("message: {}", message);
+        println!("cmd: {}, player_index: {:?}", cmd, *PLAYER_INDEX.lock().unwrap());
     }
+}
+
+fn main() {
+    thread::spawn(run_visualization);
+    thread::spawn(run_bot);
+    run_server();
 }
