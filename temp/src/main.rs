fc15fc2708cdba6270f7dd7d5606c547a64e06f6
Ehsanul Hoque <ehsanul@ehsanul.com>
2018-09-28 23:00:07 -0700

    measure via set game state

--- temp/src/main.rs
+++ temp/src/main.rs
@@ -1,24 +1,53 @@
 extern crate rlbot;
 extern crate state;
 extern crate csv;
-extern crate passthrough;
+extern crate flatbuffers;
 
+use rlbot::flat;
 use rlbot::ffi::{ MatchSettings, LiveDataPacket, PlayerInput };
 use state::*;
 use std::thread;
 use std::time::Duration;
 use std::error::Error;
-use passthrough::{Gilrs, Gamepad, human_input, update_gamepad};
+use std::f32::consts::PI;
+
+const MAX_BOOST_SPEED: i16 = 2300;
+const MAX_ANGULAR_SPEED: i16 = 6; // TODO check
 
 struct RecordState {
-    min_speed: f32,
+    speed: i16,
+    angular_speed: i16,
     started: bool,
     records: Vec<(f32, PlayerState)>,
 }
 
 impl RecordState {
+    pub fn record(&mut self, packet: &LiveDataPacket) {
+        let mut game_state = GameState::default();
+        state::update_game_state(&mut game_state, &packet, 0);
+        if !self.is_started(&game_state) { return }
+
+        let latest = (packet.GameInfo.TimeSeconds, game_state.player.clone());
+        if self.records.len() > 0 {
+            let last = self.records[self.records.len() - 1];
+            if last != latest {
+                self.records.push(latest);
+            }
+        } else {
+            self.records.push(latest);
+        }
+    }
+
+    fn is_started(&self, game_state: &GameState) -> bool {
+        let pos_threshold = 5.0;
+        let speed_threshold = 0.1;
+        let pos = game_state.player.position;
+        let speed = game_state.player.velocity.norm();
+        pos.x.abs() < pos_threshold && pos.y.abs() < pos_threshold && speed < speed_threshold
+    }
+
     pub fn save_and_advance(&mut self) {
-        let path = format!("./throttle_left/{}.csv", self.min_speed);
+        let path = format!("./throttle_left/{}_{}.csv", self.speed, self.angular_speed);
         let mut wtr = csv::Writer::from_path(path).expect("couldn't open file for writing csv");
 
         for (t, player) in &self.records {
@@ -37,33 +66,103 @@ impl RecordState {
         }
 
         self.records.clear();
-        self.min_speed += 100.0;
-    }
-}
-
-const MAX_BOOST_SPEED: f32 = 2300.0;
-
-fn bot_input(packet: &LiveDataPacket, record_state: &mut RecordState) -> PlayerInput {
-    let mut game_state = GameState::default();
-    state::update_game_state(&mut game_state, &packet, 0);
-
-    let mut input = PlayerInput::default();
-    input.Throttle = 1.0;
-
-    if !record_state.started {
-        if game_state.player.velocity.norm() > record_state.min_speed {
-            record_state.started = true;
-        } else if record_state.min_speed > 1500.0 {
-            input.Boost = true;
+        self.speed += 100;
+        if self.speed > MAX_BOOST_SPEED {
+            self.speed = 0;
+            self.angular_speed += 1;
         }
     }
 
-    if record_state.started {
-        input.Steer = -1.0;
+    pub fn set_next_game_state(&mut self, rlbot: &rlbot::RLBot) -> Result<(), Box<Error>> {
+        self.started = false;
+
+        let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
+        let mut car_offsets = Vec::with_capacity(1);
+
+        let position = flat::Vector3Partial::create(
+            &mut builder,
+            &flat::Vector3PartialArgs {
+                x: Some(&flat::Float::new(0.0)),
+                y: Some(&flat::Float::new(0.0)),
+                z: Some(&flat::Float::new(18.0)),
+            },
+        );
+
+        let velocity = flat::Vector3Partial::create(
+            &mut builder,
+            &flat::Vector3PartialArgs {
+                x: Some(&flat::Float::new(0.0)),
+                y: Some(&flat::Float::new(self.speed as f32)),
+                z: Some(&flat::Float::new(0.0)),
+            },
+        );
+
+        let angular_velocity = flat::Vector3Partial::create(
+            &mut builder,
+            &flat::Vector3PartialArgs {
+                x: Some(&flat::Float::new(0.0)),
+                y: Some(&flat::Float::new(0.0)),
+                z: Some(&flat::Float::new(self.angular_speed as f32)),
+            },
+        );
+
+        let rotation = flat::RotatorPartial::create(
+            &mut builder,
+            &flat::RotatorPartialArgs {
+                pitch: Some(&flat::Float::new(0.0)),
+                yaw: Some(&flat::Float::new(PI/2.0)),
+                roll: Some(&flat::Float::new(0.0)),
+            },
+        );
+
+        let physics = flat::DesiredPhysics::create(
+            &mut builder,
+            &flat::DesiredPhysicsArgs {
+                location: Some(position),
+                rotation: Some(rotation),
+                velocity: Some(velocity),
+                angularVelocity: Some(angular_velocity),
+                ..Default::default()
+            },
+        );
+
+        let car_state = flat::DesiredCarState::create(
+            &mut builder,
+            &flat::DesiredCarStateArgs {
+                physics: Some(physics),
+                ..Default::default()
+            },
+        );
+        car_offsets.push(car_state);
+        let car_states = builder.create_vector(&car_offsets);
+
+        let desired_game_state = flat::DesiredGameState::create(
+            &mut builder,
+            &flat::DesiredGameStateArgs {
+                carStates: Some(car_states),
+                ..Default::default()
+            },
+        );
+
+        builder.finish(desired_game_state, None);
+        rlbot.set_game_state(&builder.finished_data())?;
+
+        Ok(())
     }
 
-    // record player
-    record_state.records.push((packet.GameInfo.TimeSeconds, game_state.player.clone()));
+    pub fn sample_complete(&self) -> bool {
+        self.records.len() > 120
+    }
+
+    pub fn all_samples_complete(&self) -> bool {
+        self.speed > MAX_BOOST_SPEED && self.angular_speed > MAX_ANGULAR_SPEED
+    }
+}
+
+fn bot_input(_packet: &LiveDataPacket, _record_state: &mut RecordState) -> PlayerInput {
+    let mut input = PlayerInput::default();
+    input.Throttle = 1.0;
+    input.Steer = -1.0;
 
     input
 }
@@ -71,13 +170,11 @@ fn bot_input(packet: &LiveDataPacket, record_state: &mut RecordState) -> PlayerI
 fn main() -> Result<(), Box<Error>> {
     let mut packet = LiveDataPacket::default();
     let mut record_state = RecordState {
-        min_speed: 0.0,
+        speed: 0,
+        angular_speed: 5 * -MAX_ANGULAR_SPEED, // 5 is our scale-up factor used to round to the nearest integer
         started: false,
         records: vec![],
     };
-    let mut gilrs = Gilrs::new().unwrap();
-    let mut gamepad = Gamepad::default();
-
 
     let rlbot = rlbot::init()?;
     let mut settings = MatchSettings {
@@ -93,25 +190,19 @@ fn main() -> Result<(), Box<Error>> {
 
     loop {
         rlbot.update_live_data_packet(&mut packet)?;
-        //println!("{}: {:?}", count, packet.GameBall);
 
-        update_gamepad(&mut gilrs, &mut gamepad);
-        let input = if gamepad.select_toggled {
-            bot_input(&packet, &mut record_state)
-        } else {
-            human_input(&gamepad)
-        };
-
-        if record_state.records.len() > 2000 {
-            gamepad.select_toggled = false;
+        record_state.record(&packet);
+        if record_state.sample_complete() {
             record_state.save_and_advance();
-            record_state.started = false;
 
-            if record_state.min_speed > MAX_BOOST_SPEED {
+            if record_state.all_samples_complete() {
                 break;
-            };
+            } else {
+                record_state.set_next_game_state(&rlbot)?;
+            }
         }
 
+        let input = bot_input(&packet, &mut record_state);
         rlbot.update_player_input(input, 0)?;
         thread::sleep(Duration::from_millis(1000/250));
     }
